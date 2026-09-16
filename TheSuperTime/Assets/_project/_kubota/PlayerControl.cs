a01576c94cd91ffe72ecb50a9b237ffda4cd76b1
kubotananato <fko2647003@stu.o-hara.ac.jp>
2026-09-16 17:59:40 +0900

    PlayerContlorCamera

---
+++ TheSuperTime/Assets/_project/_kubota/PlayerControl.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = 0f;
+        float vertical = 0f;
+
+        // WASD移動
+        if (Keyboard.current.aKey.isPressed)
+        {
+            horizontal = -1f;
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            horizontal = 1f;
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            vertical = -1f;
+        }
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            vertical = 1f;
+        }
+
+        // Playerの向いている方向を基準にする
+        Vector3 move =
+            transform.forward * vertical +
+            transform.right * horizontal;
+
+        // 斜め移動が速くならないようにする
+        if (move.magnitude > 1f)
+        {
+            move.Normalize();
+        }
+
+        // 移動
+        transform.position += move * MoveSpeed * Time.deltaTime;
+
+    }
+}
